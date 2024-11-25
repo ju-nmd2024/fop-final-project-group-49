654acfe849e4cbd66ff7c43dad89f6e5cfc55989
Jegrid <79661901+Jegrid@users.noreply.github.com>
2024-11-25 23:06:49 +0100

    Added doodleguy

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
+class DoodleGuy {
+  constructor() {
+    this.x = 200;
+    this.y = 500;
+    this.height = 40;
+    this.width = 60;
+  }
+}
+
+let doodleguy;
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(400, 600);
+  doodleguy = new DoodleGuy();
 }
 
 function draw() {
-  background(255, 140, 0);
+  background(0);
+
+  //Draw doodleguy
+  rect(doodleguy.x, doodleguy.y, doodleguy.height, doodleguy.width);
 }
